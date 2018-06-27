fd8521536179ceafa1def72939f52a09d85543cc
Yass_boss <s.s.yaskevych@gmail.com>
2018-06-27 12:59:10 +0300

    init commit

---
+++ app/src/main/java/com/example/ass_boss/geoquiz/QuizActivity.java
@@ -0,0 +1,49 @@
+package com.example.ass_boss.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.security.interfaces.RSAKey;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        trueButton = (Button) findViewById(R.id.true_button);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast toast = Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT);
+
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
+            }
+        });
+        falseButton = (Button) findViewById(R.id.false_button);
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT);
+
+                toast.setGravity(Gravity.TOP, 0, 0);
+
+                toast.show();
+            }
+        });
+    }
+}
